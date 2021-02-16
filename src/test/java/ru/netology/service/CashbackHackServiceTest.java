ae7601a2f4914b200e4b98093a262b97be4e3675
Sergei Ovchinnikov <s.y.ovchinnikov@anoitr.ru>
2021-02-16 20:05:55 +0300

    yes

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,4 +11,23 @@ public class CashbackHackServiceTest {
         assertEquals(2,2);
     }
 
+    @Test
+    public void Test1() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+
+        assertEquals(actual, 100 );
+
+    }
+
+    @Test
+    public void Test2() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+
+        assertEquals( actual, 0);
+
+
+    }
+
 }
